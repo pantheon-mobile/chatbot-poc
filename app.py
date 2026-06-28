8f2e9fdae2bb877adbf5ca152b3fd952ab7a654d
pantheon-mobile <kasai@pantheon-mobile.com>
2026-06-28 21:39:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ with tab1:
             st.markdown(message["content"])
 
     # ユーザーからの質問入力
-user_query = st.chat_input("例：学生寮に入っている場合の申請書類を教えてください")
+    user_query = st.chat_input("例：学生寮に入っている場合の申請書類を教えてください")
     if user_query:
         with st.chat_message("user"):
             st.markdown(user_query)
